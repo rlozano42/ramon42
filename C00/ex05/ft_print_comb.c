471e3ac0e85f9625e69976eb5f7c8906d0179c58
rlozano42 <55979893+rlozano42@users.noreply.github.com>
2019-10-06 16:42:46 +0200

    ex04  y ex05

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlozano <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/06 12:20:04 by rlozano           #+#    #+#             */
+/*   Updated: 2019/10/06 15:23:37 by rlozano          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print(char a, char b, char c);
+
+void	ft_print_comb(void)
+{
+	char a;
+	char b;
+	char c;
+
+	a = '0';
+	b = '1';
+	c = '2';
+	while (a <= '7')
+	{
+		ft_print(a, b, c, d);
+		if (c == '9' && b != '8')
+		{
+			b++;
+			c = b + 1;
+		}
+		else if (c == '9' && b == '8' && a <= '7')
+		{
+			a++;
+			c = b + 1;
+			b = a + 1;
+		}
+		else
+			c++;
+	}
+}
+
+void	ft_print(char a, char b, char c)
+{
+	write(1, &a, 1);
+	write(1, &b, 1);
+	write(1, &c, 1);
+	write(1, ", ", 2);
+}
