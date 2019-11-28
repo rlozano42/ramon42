9798bf8f06542c380515eaa13e2b0b531c14c7c4
rlozano42 <55979893+rlozano42@users.noreply.github.com>
2019-11-28 20:44:00 +0100

    itoa

--- cursus42/ft_itoa.c
+++ cursus42/ft_itoa.c
@@ -6,24 +6,55 @@
 /*   By: rlozano <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/26 18:35:50 by rlozano           #+#    #+#             */
-/*   Updated: 2019/11/26 19:37:15 by rlozano          ###   ########.fr       */
+/*   Updated: 2019/11/28 13:30:12 by rlozano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_itoa(int n)
+static int			ft_size(long int nb)
 {
-	int *str;
-	
-	str = (int *)n;
-	while (str[i] == '\t' || str[i] == '\n' ||
-			str[i] == '\v' || str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
-		str++;
-	while (str[i] == '+' || str[i] == '-')
+	long int digit_n;
+
+	digit_n = 1;
+	if (nb == 0)
+		return (1);
+	if (nb <= 10)
 	{
-		if (str[i] == '-')
-			sign = sign * -1;
-		str++;
+		nb = nb * -1;
+		digit_n = digit_n + 1;
 	}
+	while (nb >= 10)
+	{
+		digit_n = digit_n + 1;
+		nb = nb / 10;
+	}
+	return (digit_n);
+}
+
+char				*ft_itoa(int x)
+{
+	char			*aux;
+	long int		n;
+	long int		size;
+
+	n = x;
+	size = ft_size(n);
+	if (!(aux = malloc(sizeof(char) * (size + 1))))
+		return (0);
+	if (n < 0)
+	{
+		aux[0] = '-';
+		n = n * -1;
+	}
+	else
+		aux[0] = 0;
+	aux[size] = '\0';
+	while (size > (aux[0] == '-' ? 1 : 0))
+	{
+		size--;
+		aux[size] = (n % 10) + '0';
+		n = n / 10;
+	}
+	return (aux);
 }
