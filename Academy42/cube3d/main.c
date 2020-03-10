726f9fac593a4eab3e7401828a3e54eb4168ed9b
Ramon Lozano <rlozano@c2r6s6.42madrid.com>
2020-03-10 17:46:55 +0100

    Coronavirus

---
+++ Academy42/cube3d/main.c
@@ -0,0 +1,90 @@
+#include "mlx.h"
+
+typedef struct  s_ram{
+    int         izq;
+    int         drch;
+    int         up;
+    int         down;
+    void        *mlx_ptr;
+    void        *win_ptr;
+    void        *img;
+    int         i;
+    int         j;
+    int         *addr;
+    int         bits_per_pixel;
+    int         line_length;
+    int         endian;
+}               t_ram;
+
+int     down(int keycode, t_ram *param)
+{
+    if (keycode == 126)
+        param->up = 1;
+    printf("Down: %d\n", keycode);
+}
+
+int     release(int keycode, t_ram *param)
+{
+    if (keycode == 126)
+        param->up = 0;
+    printf("Release: %d\n", keycode);
+}
+
+void    ft_init(t_ram *param)
+{
+    param->izq = 0;
+    param->drch = 0;
+    param->up = 0;
+    param->down = 0;
+    param->j = 0;
+    param->i = 0;
+}
+
+int test2(t_ram *param)
+{
+    if (param->up == 1)
+    {
+        param->j = 0;
+        while (param->j <= 799)
+        {
+            param->i = 0;
+            while (param->i <= 799)
+                param->addr[param->j * 800 + param->i++] = 0xFF0000; // (x, y) ---> y * ancho + x
+            param->j++;
+        }
+        mlx_put_image_to_window(param->mlx_ptr, param->win_ptr, param->img, 0, 0);
+    }
+    else
+    {
+        param->j = 0;
+        while (param->j <= 799)
+        {
+            param->i = 0;
+            while (param->i <= 799)
+                param->addr[param->j * 800 + param->i++] = 0xFFFFFF; // (x, y) ---> y * ancho + x
+            param->j++;
+        }
+        mlx_put_image_to_window(param->mlx_ptr, param->win_ptr, param->img, 0, 0);
+    }
+}
+
+int main()
+{
+    t_ram param;
+
+    ft_init(&param);
+    param.mlx_ptr = mlx_init();
+    param.win_ptr = mlx_new_window(param.mlx_ptr, 800, 800, "mlx 42");
+
+    param.img = mlx_new_image(param.mlx_ptr, 800, 800);
+
+    param.addr = (int *)mlx_get_data_addr(param.img, &param.bits_per_pixel, &param.line_length,&param.endian);
+
+ /*   while (param.i < 800)
+        param.addr[400 * 800 + param.i++] = 0xFF0000; // (x, y) ---> y * ancho + x*/
+
+    mlx_hook(param.win_ptr, 2, 0, down, &param);
+    mlx_hook(param.win_ptr, 3, 0, release, &param);
+    mlx_loop_hook(param.mlx_ptr, test2, &param);
+    mlx_loop(param.mlx_ptr);
+}
