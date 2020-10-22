360e1f2efdcf249af5cb5bc2951e17ca41f77dc6
Ramon Lozano <rlozano@c2r12s1.42madrid.com>
2020-10-22 09:03:52 +0200

    Backup

--- Academy42/cube3d/cube3d.h
+++ Academy42/cube3d/cube3d.h
@@ -3,19 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   cube3d.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rlozano <rlozano@student.42madrid.com      +#+  +:+       +#+        */
+/*   By: rlozano <rlozano@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/24 18:46:22 by rlozano           #+#    #+#             */
-/*   Updated: 2020/07/07 19:58:09 by rlozano          ###   ########.fr       */
+/*   Updated: 2020/10/21 14:11:20 by rlozano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUBE3D_H
 #define CUBE3D_H
 
-//#include "minilibx-linux/mlx.h"
-#include "GNL/get_next_line.h"
-#include "ft_printf/ft_printf.h"
+#include "includes/minilibx/mlx.h"
+#include "includes/GNL/get_next_line.h"
+#include "includes/ft_printf/ft_printf.h"
 #include <stdio.h>
 #include <fcntl.h>
 #include <string.h>
@@ -34,10 +34,28 @@ typedef struct	s_cam{
 	int			endian;
 }				t_cam;
 
+typedef struct s_man
+{
+	int		fd;
+	char	*line;
+	int		resolution_x;
+	int		resolution_y;
+	char	*north;
+	char	*south;
+	char	*west;
+	char	*east;
+	char	*sprite;
+	char	**aux;
+	int 	floor[3];
+	int		ceiling[3];
+}				t_man;
+
+
 //void 			ft_init(t_cam *param);
 //int				down(int keycode, t_cam *param);
 //int				release(int keycode, t_cam *param);
 //int				test2(t_cam *param);
+void			ft_handleresolution(t_man *param);
 int				is_digit(char c);
 int				is_space(char c);
 
--- Academy42/cube3d/readmap.c
+++ Academy42/cube3d/readmap.c
@@ -3,48 +3,98 @@
 /*                                                        :::      ::::::::   */
 /*   readmap.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rlozano <rlozano@student.42madrid.com      +#+  +:+       +#+        */
+/*   By: rlozano <rlozano@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/24 20:08:55 by rlozano           #+#    #+#             */
-/*   Updated: 2020/07/08 19:18:29 by rlozano          ###   ########.fr       */
+/*   Updated: 2020/10/21 14:11:43 by rlozano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube3d.h"
 #include <string.h>
 
+void	ft_handleresolution(t_man *param)
+{
+			param->aux = ft_split(param->line, ' ');
+			param->resolution_x = atoi(param->aux[1]);
+			param->resolution_y = atoi(param->aux[2]);
+			printf("X = %d\nY = %d\n", param->resolution_x, param->resolution_y);
+}
+
 int main(int argc, char **argv)
 {
-	int		fd;
-	char	*line;
-	int		resolution_x;
-	int		resolution_y;
-	char	*no;
+	t_man param;	
 
 	argc = 1;
-	fd = open(argv[1], O_RDONLY);
-	while (get_next_line(fd, &line) > 0)
+	param.fd = open(argv[1], O_RDONLY);
+	while (get_next_line(param.fd, &param.line) > 0)
 	{
-		if ((line = ft_strchr(line, 'R')) != NULL)
+		if ((ft_strchr(param.line, 'R')) != NULL)
+			ft_handleresolution(&param);
+		else if (ft_strchr(param.line, 'N') && ft_strchr(++param.line, 'O') != NULL)
 		{
-			line++;
-			resolution_x = atoi(line);
-			while (is_space(*line))
-				line++;
-			while (is_digit(*line))
-				line++;
-			resolution_y = atoi(line);
-			printf("X = %d\nY = %d\n", resolution_x, resolution_y);
+			param.aux = ft_split(param.line, ' ');
+			param.north = param.aux[1];
+			printf("NO = %s\n", param.north);
 		}
-/*		else if(*line == 'N' && *(line + 1) == 'O')
+		else if (ft_strnstr(param.line, "SO", 2) != NULL)
 		{
-			while (is_space(*line))
-				line++;
-			if (*line++ == '.' && *line == '/')
-			{
-				no = ft_strdup(line);	
-			}
-			printf("NO = %s\n", no);
-		}*/
+			param.aux = ft_split(param.line, ' ');
+			param.south = param.aux[1];
+			printf("SO = %s\n", param.south);
+		}
+		else if (ft_strchr(param.line, 'W') && ft_strchr(++param.line, 'E') != NULL)
+		{
+			param.aux = ft_split(param.line, ' ');
+			param.west = param.aux[1];
+			printf("WE = %s\n", param.west);
+		}
+		else if (ft_strchr(param.line, 'E') && ft_strchr(++param.line, 'A') != NULL)
+		{
+			param.aux = ft_split(param.line, ' ');
+			param.east = param.aux[1];
+			printf("EA = %s\n", param.east);
+		}
+		else if (ft_strnstr(param.line, "S ", 2) != NULL)
+		{
+			param.aux = ft_split(param.line, ' ');
+			param.sprite = param.aux[1];
+			printf("S = %s\n", param.aux[1]);
+		}
+		else if ((ft_strchr(param.line, 'F')) != NULL)
+		{
+			param.line++;
+			param.floor[0] = atoi(param.line);
+			printf("0 = %d\n", param.floor[0]);
+			while (*param.line != ',')
+				param.line++;
+			param.line++;
+			param.floor[1] = atoi(param.line);
+			printf("1 = %d\n", param.floor[1]);
+			while (*param.line != ',')
+				param.line++;
+			param.line++;
+			param.floor[2] = atoi(param.line);
+			printf("2 = %d\n", param.floor[2]);
+		}
+		else if ((ft_strchr(param.line, 'C')) != NULL)
+		{
+			param.line++;
+			param.ceiling[0] = atoi(param.line);
+			printf("0 = %d\n", param.ceiling[0]);
+			while (*param.line != ',')
+				param.line++;
+			param.line++;
+			param.ceiling[1] = atoi(param.line);
+			printf("1 = %d\n", param.ceiling[1]);
+			while (*param.line != ',')
+				param.line++;
+			param.line++;
+			param.ceiling[2] = atoi(param.line);
+			printf("2 = %d\n", param.ceiling[2]);
+		}
+		
 	}
 }
+
+
